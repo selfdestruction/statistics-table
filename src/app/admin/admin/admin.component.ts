bf205f8dd6cb5c9f0d30fdd87ed091eead6c3c65
selfdestruction <vicktor.siryack@gmail.com>
2016-12-08 15:05:38 +0200

    add creepy route animation (fix it later)

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {routerTransition} from "../../router.animations";
 
 @Component({
   selector: 'app-admin',
+  animations: [routerTransition()],
+  host: {'[@routerTransition]': ''},
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HighchartsService } from "./services/highcharts.service.ts";
 import { APIService } from "./services/api.service.ts";
+import { routerTransition } from './router.animations';
 
 @Component({
   selector: 'app-root',
+  animations: [routerTransition()],
+  host: {'[@routerTransition]': ''},
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
